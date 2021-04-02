e71174a11242a3b26d01e84192b178b8cfe5d1d7
Tyler Fabian <tylerf808@gmail.com>
2021-04-02 17:38:21 -0400

    Fixed logic of updating url and added functionality to date selector

--- assets/graph.js
+++ assets/graph.js
@@ -4,26 +4,75 @@ var redditPostsArray = [];
 var labelsArray = [];
 var numDays = 5;
 var chart;
+var startDay, endDay;
 
 //Store html elements into variables
 var fiveDayBtn = document.getElementById('5day');
 var thirtyDayBtn = document.getElementById('30day');
 var ninetyDayBtn = document.getElementById('90day');
 var oneYearBtn = document.getElementById('365day');
+var startText = document.getElementById('startDateInput');
+var endText = document.getElementById('endDateInput');
+var submitBtn = document.getElementById('submitBtn')
 
 //Add onclick functions to each button that changes the number of days and the a function that updates the api url
-fiveDayBtn.onclick = function () { numDays = 5; updateGraph();}
-thirtyDayBtn.onclick = function () { numDays = 30; updateGraph();}
-ninetyDayBtn.onclick = function () { numDays = 90; updateGraph();}
-oneYearBtn.onclick = function () { numDays = 365; updateGraph();}
+fiveDayBtn.onclick = function() {
+    numDays = 5;
+    apiUrlCrypto = "https://api.lunarcrush.com/v2?data=assets&key=oaaa04joylg6nojvrsywq&symbol=BTC&data_points=5&interval=day";
+    chart.destroy();
+    redditPostsArray = [];
+    priceArray = [];
+    labelsArray = [];
+    getData();
+}
+thirtyDayBtn.onclick = function() {
+    numDays = 30;
+    apiUrlCrypto = "https://api.lunarcrush.com/v2?data=assets&key=oaaa04joylg6nojvrsywq&symbol=BTC&data_points=30&interval=day";
+    chart.destroy();
+    redditPostsArray = [];
+    priceArray = [];
+    labelsArray = [];
+    getData();
+}
+ninetyDayBtn.onclick = function() {
+    numDays = 90;
+    apiUrlCrypto = "https://api.lunarcrush.com/v2?data=assets&key=oaaa04joylg6nojvrsywq&symbol=BTC&data_points=90&interval=day";
+    chart.destroy();
+    redditPostsArray = [];
+    priceArray = [];
+    labelsArray = [];
+    getData();
+}
+oneYearBtn.onclick = function() {
+    numDays = 365;
+    apiUrlCrypto = "https://api.lunarcrush.com/v2?data=assets&key=oaaa04joylg6nojvrsywq&symbol=BTC&data_points=365&interval=day";
+    chart.destroy();
+    redditPostsArray = [];
+    priceArray = [];
+    labelsArray = [];
+    getData();
+}
+submitBtn.onclick = function() {
+    startDay = moment(startText.value, "M-D-YYYY").format("X");
+    endDay = moment(endText.value, "M-D-YYYY").format("X");
+    var dataPoints = (moment.unix(endDay) - moment.unix(startDay)) / 86400;
+    apiUrlCrypto = "https://api.lunarcrush.com/v2?data=assets&key=oaaa04joylg6nojvrsywq&symbol=BTC&interval=day&start=" + startDay +
+        "&end=" + endDay + "&data_points=" + dataPoints;
+    chart.destroy();
+    redditPostsArray = [];
+    priceArray = [];
+    labelsArray = [];
+    getData();
+
+}
 
 //Fetch call the API to get price data
 function getData() {
     fetch(apiUrlCrypto)
-        .then(function (response) {
+        .then(function(response) {
             return response.json();
         })
-        .then(function (da) {
+        .then(function(da) {
             for (var i = 0; i < da.data[0].timeSeries.length; i++) {
                 var objTemp = da.data[0].timeSeries[i];
                 var numTemp = objTemp.close;
@@ -35,25 +84,6 @@ function getData() {
             }
             createChart();
         });
-}
-
-//Changes graph on btn press
-function updateGraph(){
-    chart.destroy();
-    redditPostsArray = [];
-    priceArray = [];
-    labelsArray = [];
-    //if statements that change url depending on how many days are selected
-    if(numDays == 5){
-        apiUrlCrypto = "https://api.lunarcrush.com/v2?data=assets&key=oaaa04joylg6nojvrsywq&symbol=BTC&data_points=5&interval=day";
-    } else if (numDays == 30){
-        apiUrlCrypto = "https://api.lunarcrush.com/v2?data=assets&key=oaaa04joylg6nojvrsywq&symbol=BTC&data_points=30&interval=day"
-    } else if (numDays == 90){
-        apiUrlCrypto = "https://api.lunarcrush.com/v2?data=assets&key=oaaa04joylg6nojvrsywq&symbol=BTC&data_points=90&interval=day"
-    }else {
-        apiUrlCrypto = "https://api.lunarcrush.com/v2?data=assets&key=oaaa04joylg6nojvrsywq&symbol=BTC&data_points=365&interval=day";
-    }
-    getData();
 }
 
 //Create and edit chart object
@@ -81,7 +111,6 @@ function createChart() {
                 data: redditPostsArray,
                 fill: false
             }],
-            
         },
 
         // Configuration options go here
@@ -95,15 +124,15 @@ function createChart() {
                         max: Math.max(...priceArray),
                         min: Math.min(...priceArray)
                     }
-                  }, {
+                }, {
                     id: 'B',
                     type: 'linear',
                     position: 'right',
                     ticks: {
-                      max: Math.max(...redditPostsArray),
-                      min: Math.min(...redditPostsArray)
+                        max: Math.max(...redditPostsArray),
+                        min: Math.min(...redditPostsArray)
                     }
-                  }]
+                }]
             }
         }
     });
