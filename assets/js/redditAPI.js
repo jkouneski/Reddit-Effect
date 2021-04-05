98c2e31de5801435a303eac6d769c0e88baaba97
Joey Kouneski <jkouneski@yahoo.com>
2021-04-05 00:00:48 -0400

    adjustments to reddit API. now works on github server and gets coin from local storage to pass to api url

--- assets/js/redditAPI.js
+++ assets/js/redditAPI.js
@@ -1,40 +1,42 @@
-const container = document.querySelector(".subreddit")
+var container = document.querySelector(".container-subreddit")
+var selectedCoin = localStorage.getItem('coin')
+var redditUrl = "https://www.reddit.com/r/"+ selectedCoin + "/top.json";
     //Function to fetch reddit API data
 function renderPosts() {
 
-    fetch(`https://www.reddit.com/r/bitcoin/top.json`)
-        .then(function(response) {
-            return response.json();
-        })
-        //
-        .then(function(response) {
+  fetch(redditUrl)
+      .then(function(response) {
+          return response.json();
+      })
+      //
+      .then(function(response) {
 
-            let currPost, markup = ``;
+        var redditPost, markup = ``;
 
-            // Array with posts                  
-            const postsArr = response.data.children;
+        // Array with posts                  
+        var postsArr = response.data.children;
 
+        //Subreddit Post card markdown title
+        markup = `<h2>Top posts from r/${coin} subreddit</h2>`;
 
-            markup = `<h3>Top posts from r/bitcoin</h3>`;
-
-            //Loops through postArr to markdown a HTML element for each reddit post obje                       
-            for (let i = 0; i < postsArr.length; i++) {
-                currPost = postsArr[i].data;
-                markup += `
-            <a class="post" href="https://www.reddit.com/${currPost.permalink}">
-               <div class="title"> ${currPost.title} </div>
-               <div class="content"> 
-                   ${currPost.selftext} 
-                   </br></br>
-                   <span>${currPost.url}</span>
-               </div>
-               <div class="author"> Posted by ${currPost.author} </div>
-            </a>
-         `;
-            }
-            // Insert the markup HTML to our container
-            container.insertAdjacentHTML('afterbegin', markup);
-        })
+        //Loops through postArr to markdown a HTML element for each reddit post obje                       
+        for (var i = 0; i < postsArr.length; i++) {
+            redditPost = postsArr[i].data;
+            markup += `
+              <div class = "cell">
+                <div class = "card">
+                  <div class="card-title card-divider">${redditPost.title}</div>
+                  <a href = "${redditPost.url}">See Post</a>
+                  <div class="card-author"> Posted by ${redditPost.author} </div>
+                </div>
+              </div>
+              `;
+        }
+        // Insert the markup HTML to our container
+        container.insertAdjacentHTML('afterbegin', markup);
+    })
 };
+  renderPosts();  
+   
 
-renderPosts();
+  
